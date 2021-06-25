7ea4c7e2009f4016e45824182d42477aaa44da11
mgeorgic <79463692+mgeorgic@users.noreply.github.com>
2021-06-25 15:50:31 -0600

    added area, width and height for chart

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+
 // @TODO: YOUR CODE HERE!
+// Resize and where to put your code for the scatter plot
+
+var svgArea = d3.select("#scatter").select("svg");
+
+// clear svg is not empty
+if (!svgArea.empty()) {
+svgArea.remove();
+}
+
+svgWidth = document.getElementById('scatter').clientWidth;
+svgHeight = svgWidth / 1.45;
+
+var border= 1.5;
+var bordercolor='dark blue';
+
